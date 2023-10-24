e7db0d1104cd7d41ff497d4ea2ce831bea51ea7a
Nasrallah04 <nasrallah.dev@gmail.com>
2023-10-24 15:48:12 +0100

    Reinitializing the project

--- server/config/database.js
+++ server/config/database.js
@@ -1,1 +1,18 @@
 // Database configuration
+
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017'
+
+, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error('MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,30 @@
 // User schema and model
+const mongoose = require('mongoose');
+
+// Define the user schema
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Create a model for the user schema
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
+
