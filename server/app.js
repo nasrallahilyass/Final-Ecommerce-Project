0ec040307e406142a7d386f15c6d46c8e8bc0684
Nasrallah04 <nasrallah.dev@gmail.com>
2023-10-25 19:26:57 +0100

    initialize the ecommerce project

--- server/app.js
+++ server/app.js
@@ -1,1 +1,33 @@
 // Main application file
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require('dotenv').config();
+
+const port = process.env.PORT || 5000; // Use the defined PORT or default to 5000
+const mongoURI = process.env.MONGODB_URI;
+
+
+
+const app = express();
+app.use(express.static('public'));
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(`${mongoURI}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB: ", err);
+  });
+
+
+
+app.listen(port, (err, res) => {
+  console.log(`server is Running : http://localhost:${port}`);
+});
--- server/config/database.js
+++ server/config/database.js
@@ -1,18 +1,21 @@
-// Database configuration
-
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017'
-
-, {
+// Database connection options
+const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+  useCreateIndex: true, // Use the new option for createIndexes
+};
 
-const db = mongoose.connection;
+// Connect to the database
+mongoose.connect(process.env.DATA_BASE_URL, options)
+  .then(() => {
+    console.log('Connected to the database');
+  })
+  .catch((error) => {
+    console.error('Database connection error:', error);
+  });
 
-db.on('error', console.error('MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
+// Export the Mongoose instance for use in your application
+module.exports = mongoose;
