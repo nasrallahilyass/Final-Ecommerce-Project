008a7813d7310e8701860bd8b8315a46e120c349
kawtar-chaanaoui <kawtar.chaanaoui@usmba.ac.ma>
2023-10-26 16:31:41 +0000

    products api

--- server/app.js
+++ server/app.js
@@ -9,8 +9,6 @@ const port = process.env.PORT || 5000; // Use the defined PORT or default to 500
 
 const app = express();
 
-connectDb()
-
 app.use(express.static('public'));
 
 app.use(cors());
@@ -19,6 +17,7 @@ app.use(express.json());
 
 app.use('/v1/products', productRoutes);
 
+connectDb();
 
 app.listen(port, (err, res) => {
   console.log(`server is Running : http://localhost:${port}`);
