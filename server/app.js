858f316e09c40be6aac0646c981e87083a307924
Imane-Ouahmane <imaneouahmane202@gmail.com>
2023-11-29 10:33:09 +0100

    seller dashboard

--- server/app.js
+++ server/app.js
@@ -3,26 +3,46 @@ require('dotenv').config();
 require('./config/database')
 const UserRouter = require("./routes/userRoutes")
 const bodyParser = require('express').json;
+const customerRoutes = require("./routes/customerRoutes.js");
+const session = require("express-session");
 const express = require("express");
 const cors = require("cors");
-const cookie = require('cookie-parser')
+const cookieParser = require('cookie-parser')
 const port = process.env.PORT || 5000; 
 
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 
 
 // Midll
 app.use(bodyParser())
-app.use(cookie())
-app.use('/v1',UserRouter)
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5173",
+    // methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    // optionsSuccessStatus: 204,
+  })
+);
 
-app.use(cors());
-app.use(express.json());
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false },
+  })
+);app.use(express.json());
+
+app.use('/v1',UserRouter)
+app.use("/customers", customerRoutes);
+
 
 
 
 app.listen(port, (err, res) => {
-  console.log(`server is Running : http://localhost:${port}`);
+  console.log(`server is Running on port ${port}`);
 });
